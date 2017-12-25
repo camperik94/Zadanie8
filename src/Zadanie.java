cfc4dd1653902b684b911b512f01653d2fb72804
camperik94 <tomaszjach94@gmail.com>
2017-12-25 13:27:55 +0100

    Dodanie Wczytywania liczb

--- src/Zadanie.java
+++ src/Zadanie.java
@@ -1,6 +1,24 @@
+import java.util.Scanner;
 
 public class Zadanie {
 	public static void main(String[] args) {
+
+		Scanner in = new Scanner(System.in);
+
+		System.out.println("Podaj dwie liczby A < B");
+
+		System.out.println("Podaj A");
 		
+		int a = in.nextInt();
+		
+		System.out.println("Podaj B");
+		
+		int b = in.nextInt();
+		
+		in.close();
+		
+		
+
+
 	}
 }
